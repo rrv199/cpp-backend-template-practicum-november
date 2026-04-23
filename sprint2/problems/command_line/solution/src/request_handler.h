10792e70230abd70a4f04475544251ca7764e83d
romas <89295077542@mail.ru>
2026-04-23 17:10:28 +0000

    Refactor command_line: extract endpoints and serialization methods

---
+++ sprint2/problems/command_line/solution/src/request_handler.h
@@ -0,0 +1,4 @@
+
+    json::array SerializeRoads(const model::Roads& roads);
+    json::array SerializeBuildings(const model::Buildings& buildings);
+    json::array SerializeOffices(const model::Offices& offices);
