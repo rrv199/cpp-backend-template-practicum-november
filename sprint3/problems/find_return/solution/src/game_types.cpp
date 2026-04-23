808946ca226b208b5c9caf49316c7683081f2685
romas <89295077542@mail.ru>
2026-04-23 16:40:02 +0000

    Add complete solutions for find_return and scores

---
+++ sprint3/problems/find_return/solution/src/game_types.cpp
@@ -0,0 +1,17 @@
+#include "game_types.h"
+
+namespace game {
+
+Player::Player(size_t bag_capacity) : bag_capacity_(bag_capacity) {}
+
+void Player::AddToBag(const Item& item) {
+    if (!IsBagFull()) {
+        bag_.push_back(item);
+    }
+}
+
+void Player::ClearBag() {
+    bag_.clear();
+}
+
+} // namespace game
